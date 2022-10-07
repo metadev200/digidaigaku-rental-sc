8ed420eb356669d0e6e2431168b2dd01f76be7eb
Nakashi Satomoto <metavdev@proton.me>
2022-10-07 23:41:14 +0800

    function: modified afterConfirmedDeposit

--- cloudcode/afterConfirmedDepositHero.js
+++ cloudcode/afterConfirmedDepositHero.js
@@ -1,35 +1,48 @@
 const logger = Moralis.Cloud.getLogger();
 
 Moralis.Cloud.afterSave("EventGenesisDeposited", async function (request) {
-    const confirmed = request.object.get("confirmed")
+    const confirmed = request.object.get("confirmed");
 
-    const genesisId = request.object.get("genesisId")
-    const genesisOwner = request.object.get("genesisOwner")
-    const fee = request.object.get("fee")
+    const genesisId = request.object.get("genesisId");
+    const genesisOwner = request.object.get("genesisOwner");
+    const fee = request.object.get("fee");
 
     if (confirmed) {
-        logger.info("Listing new genesis")
+        const existingListing = await getGenesisListing(genesisId, genesisOwner);
+        if (existingListing) {
+            logger.info("Re-listing genesis");
+            existingListing.set("withdrawn", false);
+            existingListing.set("fee", fee);
 
-        const GenesisListing = Moralis.Object.extend("GenesisListing");
-        const genesisListing = new GenesisListing();
-    
-        const params = {
-            genesisId,
-            genesisOwner,
-            fee,
-        }
+            existingListing.save();
+        } else {
+            logger.info("Listing new genesis");
 
-        const httpResponse = await Moralis.Cloud.httpRequest({
-            url: `https://digidaigaku.com/metadata/${genesisId}.json`,
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
+            const GenesisListing = Moralis.Object.extend("GenesisListing");
+            const genesisListing = new GenesisListing();
+            const withdrawn = false;
+            const onQuest = false;
+
+            const params = {
+                genesisId,
+                genesisOwner,
+                fee,
+                withdrawn,
+                onQuest
             }
-        })
 
-        const metadata = JSON.parse(httpResponse.text)
-        params.metadata = metadata.name
-        params.image = metadata.image
+            const httpResponse = await Moralis.Cloud.httpRequest({
+                url: `https://digidaigaku.com/metadata/${genesisId}.json`,
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                }
+            })
 
-        await genesisListing.save(params)
+            const metadata = JSON.parse(httpResponse.text);
+            params.metadata = metadata.name;
+            params.image = metadata.image;
+
+            await genesisListing.save(params);
+        }
     }
 })
