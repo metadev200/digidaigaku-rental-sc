927e4e15526454b2504441516f2e12d52413d460
Nakashi Satomoto <metavdev@proton.me>
2022-10-07 23:41:13 +0800

    feat: added force claim and reset state after minting

--- contracts/BreederDigiRenter.sol
+++ contracts/BreederDigiRenter.sol
@@ -19,14 +19,21 @@ contract BreederDigiRenter is Context {
         HeroAdventure(0xE60fE8C4C60Fd97f939F5136cCeb7c41EaaA624d);
     ERC20 weth = ERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
 
+    uint256 public constant FORCE_CLAIM_WINDOW = 1 days;
+    uint256 public constant HERO_QUEST_DURATION = 1 days;
+
     mapping(uint16 => uint256) public genesisFee;
 
     mapping(uint16 => bool) public genesisIsDeposited;
     mapping(uint16 => bool) public genesisIsOnAdventure;
-    mapping(uint16 => address) private _genesisOwner;
 
+    mapping(uint16 => address) private _genesisOwner;
     mapping(uint16 => address) private _spiritOwner;
+
     mapping(uint16 => uint16) private _spiritGenesisAdventurePair;
+    mapping(uint16 => uint16) private _genesisSpiritAdventurePair;
+
+    mapping(uint16 => uint256) private _genesisQuestStart;
 
     event GenesisDeposited(uint16 genesisId, address genesisOwner, uint256 fee);
     event GenesisWithdrawn(uint16 genesisId, address genesisOwner);
@@ -57,10 +64,14 @@ contract BreederDigiRenter is Context {
         _;
     }
 
-    modifier onlyGenesisDeposited(uint16 genesisId) {
+    modifier onlyGenesisAvailable(uint16 genesisId) {
         require(
             genesisIsDeposited[genesisId],
-            "BreederDigiRenter.onlyGenesisDeposited: genesis not deposited"
+            "BreederDigiRenter.onlyGenesisAvailable: genesis not deposited"
+        );
+        require(
+            !genesisIsOnAdventure[genesisId],
+            "BreederDigiRenter.onlyGenesisAvailable: genesis is on adventure"
         );
         _;
     }
@@ -76,14 +87,9 @@ contract BreederDigiRenter is Context {
 
     function withdrawGenesis(uint16 genesisId)
         external
+        onlyGenesisAvailable(genesisId)
         onlyGenesisOwner(genesisId)
-        onlyGenesisDeposited(genesisId)
     {
-        require(
-            !genesisIsOnAdventure[genesisId],
-            "BreederDigiRenter.withdrawGenesis: genesis is on adventure"
-        );
-
         genesisToken.transferFrom(address(this), _msgSender(), genesisId);
         _genesisOwner[genesisId] = address(0);
         genesisFee[genesisId] = 0;
@@ -94,12 +100,9 @@ contract BreederDigiRenter is Context {
 
     function updateGenesisFee(uint16 genesisId, uint256 newFee)
         external
+        onlyGenesisAvailable(genesisId)
         onlyGenesisOwner(genesisId)
     {
-        require(
-            !genesisIsOnAdventure[genesisId],
-            "BreederDigiRenter.updateGenesisFee: genesis is on adventure"
-        );
         uint256 oldFee = genesisFee[genesisId];
         genesisFee[genesisId] = newFee;
 
@@ -113,15 +116,13 @@ contract BreederDigiRenter is Context {
         uint16 spiritId,
         uint16 genesisId,
         uint256 fee
-    ) external onlyGenesisDeposited(genesisId) {
+    )
+        external
+        onlyGenesisAvailable(genesisId)
+    {
         require(
             spiritToken.ownerOf(spiritId) == _msgSender(),
             "BreederDigiRenter.enterHeroQuest: not owner of spirit"
-        );
-
-        require(
-            !genesisIsOnAdventure[genesisId],
-            "BreederDigiRenter.enterHeroQuest: genesis is on adventure"
         );
 
         require(
@@ -131,7 +132,9 @@ contract BreederDigiRenter is Context {
 
         _spiritOwner[spiritId] = _msgSender();
         genesisIsOnAdventure[genesisId] = true;
+        _genesisSpiritAdventurePair[genesisId] = spiritId;
         _spiritGenesisAdventurePair[spiritId] = genesisId;
+        _genesisQuestStart[genesisId] = block.timestamp;
 
         spiritToken.transferFrom(_msgSender(), address(this), spiritId);
         genesisToken.approve(address(adventure), genesisId);
@@ -146,15 +149,56 @@ contract BreederDigiRenter is Context {
 
         require(
             genesisIsOnAdventure[genesisId],
-            "BreederDigiRenter.enterHeroQuest: genesis is not on adventure"
+            "BreederDigiRenter.mintHero: genesis is not on adventure"
         );
 
+        // set values back to zero
+        _spiritOwner[spiritId] = address(0);
         genesisIsOnAdventure[genesisId] = false;
+        _genesisSpiritAdventurePair[genesisId] = uint16(0);
+        _spiritGenesisAdventurePair[spiritId] = uint16(0);
+        _genesisQuestStart[genesisId] = 0;
 
         adventure.exitQuest(spiritId, true);
-
         heroToken.transferFrom(address(this), _msgSender(), spiritId);
 
         emit HeroMinted(spiritId, genesisId, _msgSender());
+    }
+
+    function forceClaim(uint16 genesisId) external {
+        uint16 spiritId = _genesisSpiritAdventurePair[genesisId];
+
+        require(
+            _msgSender() == _genesisOwner[genesisId],
+            "BreederDigiRenter.forceClaim: not owner of genesis"
+        );
+
+        require(
+            genesisIsOnAdventure[genesisId],
+            "BreederDigiRenter.forceClaim: genesis is not on adventure"
+        );
+
+        require(
+            _genesisQuestStart[genesisId] +
+                HERO_QUEST_DURATION +
+                FORCE_CLAIM_WINDOW <
+                block.timestamp,
+            "BreederDigiRenter.forceClaim: force claim window not yet active"
+        );
+
+        address spiritOwner = _spiritOwner[spiritId];
+
+        // set values back to zero
+        _spiritOwner[spiritId] = address(0);
+        genesisIsOnAdventure[genesisId] = false;
+        _genesisSpiritAdventurePair[genesisId] = uint16(0);
+        _spiritGenesisAdventurePair[spiritId] = uint16(0);
+        _genesisQuestStart[genesisId] = 0;
+
+        adventure.exitQuest(spiritId, true);
+        heroToken.transferFrom(address(this), spiritOwner, spiritId);
+
+        emit HeroMinted(spiritId, genesisId, spiritOwner);
+        emit ForceClaim(spiritId, genesisId, _msgSender());
     }
 }
