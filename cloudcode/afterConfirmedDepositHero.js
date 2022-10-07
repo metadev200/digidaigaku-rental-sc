0c0b219a8c93e8c4563062483c4f4c41f3fe234e
Nakashi Satomoto <metavdev@proton.me>
2022-10-07 23:41:14 +0800

    function: afterConfirmedDeposit

---
+++ cloudcode/afterConfirmedDepositHero.js
@@ -0,0 +1,35 @@
+const logger = Moralis.Cloud.getLogger();
+
+Moralis.Cloud.afterSave("EventGenesisDeposited", async function (request) {
+    const confirmed = request.object.get("confirmed")
+
+    const genesisId = request.object.get("genesisId")
+    const genesisOwner = request.object.get("genesisOwner")
+    const fee = request.object.get("fee")
+
+    if (confirmed) {
+        logger.info("Listing new genesis")
+
+        const GenesisListing = Moralis.Object.extend("GenesisListing");
+        const genesisListing = new GenesisListing();
+    
+        const params = {
+            genesisId,
+            genesisOwner,
+            fee,
+        }
+
+        const httpResponse = await Moralis.Cloud.httpRequest({
+            url: `https://digidaigaku.com/metadata/${genesisId}.json`,
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            }
+        })
+
+        const metadata = JSON.parse(httpResponse.text)
+        params.metadata = metadata.name
+        params.image = metadata.image
+
+        await genesisListing.save(params)
+    }
+})
