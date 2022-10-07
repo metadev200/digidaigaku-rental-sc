818647b2ef35af63f281bb45be88a6bdf5db305b
Nakashi Satomoto <metavdev@proton.me>
2022-10-07 23:41:13 +0800

    setup config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,36 @@
+require('dotenv').config()
 require("@nomicfoundation/hardhat-toolbox");
+require('hardhat-deploy')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: {
+    version: '0.8.9',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL || '',
+      accounts:
+        process.env.GOERLI_PRIVATE_KEY !== undefined ? [process.env.GOERLI_PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: 'USD',
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+  },
 };
